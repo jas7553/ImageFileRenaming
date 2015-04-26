764cb89c167e4e0cee31273f11bd7966a117e4a0
Jason A Smith <jas7553@gmail.com>
2015-04-25 17:46:35 -0700

    Add -r, -f, -v, and -h arguments
    usage: ImageFileRenaming.py [-h] [-r] [-v] [FILE [FILE ...]]
    
    positional arguments:
        FILE
    
    optional arguments:
        -h, --help      Show this help message and exit
        -r, --recursive Read all files under each directory, recursively.
        -f, --force     Don't prompt, just rename
        -v, --verbose   Be extra verbose.

--- src/ImageFileRenaming.py
+++ src/ImageFileRenaming.py
@@ -2,13 +2,10 @@
 A set of tools for renaming image files based on when the image was captured.
 
 @author: Jason A Smith
-
-@var PROMPT: Whether or not to ask before renaming a file
-@type PROMPT: bool
 """
+import argparse
 import os
 import shutil
-import sys
 
 from datetime import datetime
 
@@ -17,9 +14,9 @@ from PIL import Image
 DATE_TIME_ORIGINAL_KEY = 36867
 DATE_TIME_ORIGINAL_FORMAT = '%Y:%m:%d %H:%M:%S'
 
-PROMPT = True
+VERBOSE = False
 
-def get_date_taken(path):
+def get_date_taken(filepath):
     """Get the timestamp representing when the image was taken.
 
     The result will be in the format of:
@@ -33,104 +30,139 @@ def get_date_taken(path):
     Reference:
         http://www.exiv2.org/tags.html
   
-    @param path: path to the image file
-    @type path: str
+    @param filepath: path to the image file
+    @type filepath: str
     @return: The timestamp representing when the image was taken
     @rtype: str or None
     """
     try:
-        date_taken_timestamp = Image.open(path)._getexif()[DATE_TIME_ORIGINAL_KEY]
+        date_taken_timestamp = Image.open(filepath)._getexif()[DATE_TIME_ORIGINAL_KEY]
     except KeyError:
         return None
 
     date_taken = datetime.strptime(date_taken_timestamp, DATE_TIME_ORIGINAL_FORMAT)
     return date_taken.strftime('%Y-%m-%d %H.%M.%S')
 
-def get_image_files_in_directory(path):
+def get_image_files_in_directory(dirpath, should_recurse=False):
     """Get all the image files contained in a directory.
 
-    @param path: path to the directory in which to search
-    @type path: str
+    @param dirpath: path to the directory in which to search
+    @type dirpath: str
     @return: The image files in the given directory
     @rtype: generator<str>
     """
-    return (file for file in get_files_in_directory(path) if is_image_file(file))
+    return (filepath for filepath
+            in get_files_in_directory(dirpath, should_recurse)
+            if is_image_file(filepath))
 
-def get_files_in_directory(path):
-    """Get all the files contained in a directory.
+def get_files_in_directory(dirpath, should_recurse=False):
+    """Gets all the filepaths in a directory.
 
-    @param path: path to the directory in which to search
-    @type path: str
+    @param dirpath: path to the directory in which to search
+    @type dirpath: str
+    @param should_recurse: Recurse into sub-directories and return those filepaths as well
+    @type should_recurse: bool
     @return: The files in the given directory
     @rtype: generator<str>
     """
-    for _, _, files in os.walk(path):
-        for file in files:
-            yield file
+    os_walk_values = os.walk(dirpath) if should_recurse else [next(os.walk(dirpath))]
+    for dirpath, _, filenames in os_walk_values:
+        for filename in filenames:
+            yield dirpath + os.sep + filename
 
-def is_image_file(path):
+def is_image_file(filepath):
     """Determine whether or not a file is an image.
 
     A file is an image if and only if the PIL.Image.open function does not
     raise an IOError.
 
-    @param path: path to the file to examine
-    @type path: str
+    @param filepath: path to the file to examine
+    @type filepath: str
     @return: True if the file is an image, False otherwise
     @rtype: bool
     """
     try:
-        Image.open(path)
+        Image.open(filepath)
         return True
     except IOError:
         return False
 
-def rename_images_by_date_taken(path):
+def rename_images_by_date_taken(dirpath, should_recurse=False, should_force=False):
+    """
+    Applies rename_image_by_date_taken to all images files in a given directory.
+
+    @param dirpath: path to the directory of images to be renamed
+    @type dirpath: str
+    @param should_force: whether or not the user should be prompted before moving the file
+    @type should_force: bool
+    """
+    for filepath in get_image_files_in_directory(dirpath, should_recurse):
+        rename_image_by_date_taken(filepath, should_force)
+
+def rename_image_by_date_taken(filepath, should_force=False):
     """Rename image files based on their DateTimeOriginal Exif metadata.
 
     For each image in the given directory, rename the file based on the date
     and time when the original image data was generated.
 
-    @param path: path to the directory of images to be renamed
-    @type path: str
+    @param filepath: path to the image file to be renamed
+    @type filepath: str
+    @param should_force: whether or not the user should be prompted before moving the file
+    @type should_force: bool
     """
-    for image_file in get_image_files_in_directory(path):
-        date_taken_timestamp = get_date_taken(image_file)
-        if date_taken_timestamp is not None:
-            _, extension = os.path.splitext(image_file)
+    if not is_image_file(filepath):
+        return
 
-            new_image_file = date_taken_timestamp + extension
+    dir_name, _ = os.path.split(filepath)
+    date_taken_timestamp = get_date_taken(filepath)
+    _, extension = os.path.splitext(filepath)
 
-            print(image_file + ' -> ' + new_image_file)
+    if date_taken_timestamp is None:
+        if VERBOSE: print('No date taken information for "' + filepath + '"')
+        return
 
-            if PROMPT:
-                response = input('Rename [y]? ').lower()
-                should_rename = response in ('', 'y', 'yes')
-            else:
-                should_rename = True
+    new_filepath = dir_name + os.sep + date_taken_timestamp + extension
 
-            if should_rename:
-                shutil.move(image_file, new_image_file)
-            else:
-                continue
-        else:
-            print('no date taken information available for ' + image_file)
+    if should_force:
+        should_rename = True
+        if VERBOSE: print('Renaming: "' + filepath + '" -> "' + new_filepath + '"')
+    else:
+        try:
+            response = input('Rename "' + filepath + '" -> "' + new_filepath + '" [n]? ').lower()
+        except (EOFError, KeyboardInterrupt):
+            exit()
+        should_rename = response in ('y', 'yes')
+
+    if should_rename:
+        shutil.move(filepath, new_filepath)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        try:
-            images_directory = input('Images directory: ')
-        except EOFError:
-            exit()
-    elif len(sys.argv) == 2:
-        images_directory = sys.argv[1]
-    else:
-        print('Usage: python ' + os.path.basename(__file__) + ' directory')
-        exit()
+    program_description = 'Rename all the image FILEs (the current directory by default), ' + \
+                          'based on when the image was captured.'
+    parser = argparse.ArgumentParser(description=program_description)
 
-    if not os.path.exists(images_directory):
-        raise FileNotFoundError('Cannot find the file specified: ' + \
-                                "'" + images_directory + "'")
+    parser.add_argument('-r', '--recursive', action='store_true',
+                        help='Read all files under each directory, recursively.')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='Do not prompt, just rename the image file(s) without asking.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Be extra verbose.')
+    parser.add_argument('files', metavar='FILE', nargs='*', default='.')
 
-    os.chdir(images_directory)
-    rename_images_by_date_taken(images_directory)
+    args = parser.parse_args()
+
+    if args.verbose:
+        VERBOSE = True
+
+    for path in args.files:
+        if not os.path.exists(path):
+            raise FileNotFoundError('Cannot find the file specified: "' + path + '"')
+
+        elif os.path.isdir(path):
+            rename_images_by_date_taken(path, should_recurse=args.recursive, should_force=args.force)
+
+        elif os.path.isfile(path):
+            rename_image_by_date_taken(path, should_force=args.force)
+
+        else:
+            raise RuntimeError('Unexpected case: path exists but is not a file or directory')
